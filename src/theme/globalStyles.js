adc7c9ac27854f13175ee37abed6f745ef6cb177
Janette Day <janette.day@civicactions.com>
2019-02-04 13:15:51 -0600

    Create a copy of bootstrap.min.css. Update version #

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import { css } from 'bootstrap.min.css'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import defaultTheme from './default'
 

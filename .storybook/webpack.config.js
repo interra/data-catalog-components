7d7b5aeba410c4d0e2c2147a5a31e95b61f55a64
Janette Day <janette.day@civicactions.com>
2019-03-11 13:51:49 -0500

    Upgrade storybook to 5.0, babel 6 to babel 7 (#25)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,38 +1,13 @@
 const path = require("path");
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
-module.exports = (storybookBaseConfig, configType) => {
-  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
-
-  // add our custom loaders
-  storybookBaseConfig.module.rules.push(
-    {
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'style-loader',
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-      ],
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|svg)$/,
-      include: path.resolve(__dirname, "../src"),
-      use: [
-        {
-          loader: 'url-loader',
-        },
-        {
-          loader: 'file-loader',
-        },
-      ],
-    }
-  );
-
-  return storybookBaseConfig;
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+      },
+    ],
+  },
 };
